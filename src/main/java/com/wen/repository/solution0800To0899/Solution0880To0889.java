b5f27ea04553e32bbbf644cc3b40ef6378198375
leopold.zw <1280252178@qq.com>
2024-06-10 21:26:48 +0800

    解决第 881 题

--- src/main/java/com/wen/repository/solution0800To0899/Solution0880To0889.java
+++ src/main/java/com/wen/repository/solution0800To0899/Solution0880To0889.java
@@ -1,8 +1,63 @@
 package com.wen.repository.solution0800To0899;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class Solution0880To0889 {
+  /**
+   * 881. Boats to Save People
+   *
+   * <p>1 <= people.length <= 5 * 10^4</p>
+   * <p>1 <= people[i] <= limit <= 3 * 10^4</p>
+   */
+  public int numRescueBoats(int[] people, int limit) {
+    Integer[] realPeople = Arrays.stream(people).boxed().toArray(Integer[]::new);
+    Arrays.sort(realPeople, (o1, o2) -> o2 - o1);
+    boolean[] checked = new boolean[realPeople.length];
+    int curIndex = 0, leftCount = realPeople.length, resultCount = 0;
+    while (leftCount > 0 && curIndex < realPeople.length) {
+      checked[curIndex] = true;
+      leftCount--;
+      int lim = limit - realPeople[curIndex];
+      if (lim >= realPeople[realPeople.length - 1]) {
+        int maxIndex = binarySearchMax(realPeople, curIndex + 1, people.length - 1, lim);
+        if (maxIndex >= 0) {
+          while (maxIndex < realPeople.length && checked[maxIndex]) {
+            maxIndex++;
+          }
+          if (maxIndex < realPeople.length) {
+            checked[maxIndex] = true;
+            leftCount--;
+          }
+        }
+      }
+      resultCount++;
+      while (curIndex < realPeople.length && checked[curIndex]) {
+        curIndex++;
+      }
+    }
+
+    return resultCount;
+  }
+
+  private int binarySearchMax(Integer[] people, int fromIndex, int toIndex, int key) {
+    if (fromIndex > toIndex) {
+      return -1;
+    }
+    if (fromIndex == toIndex) {
+      return fromIndex;
+    }
+    int middle = (fromIndex + toIndex) / 2;
+    if (people[middle] > key) {
+      return binarySearchMax(people, middle + 1, toIndex, key);
+    } else {
+      if (middle - 1 < fromIndex || people[middle - 1] > key) {
+        return middle;
+      } else {
+        return binarySearchMax(people, fromIndex, middle - 1, key);
+      }
+    }
+  }
 
   /**
    * 888. Fair Candy Swap
--- src/test/java/com/wen/repository/solution0800To0899/Solution0880To0889Test.java
+++ src/test/java/com/wen/repository/solution0800To0899/Solution0880To0889Test.java
@@ -8,6 +8,19 @@ public class Solution0880To0889Test {
   Solution0880To0889 solution = new Solution0880To0889();
 
   @Test
+  public void numRescueBoats() {
+//    Assert.assertEquals(1, solution.numRescueBoats(new int[]{1, 2}, 3));
+//    Assert.assertEquals(3, solution.numRescueBoats(new int[]{3, 2, 2, 1}, 3));
+//    Assert.assertEquals(4, solution.numRescueBoats(new int[]{3, 5, 3, 4}, 5));
+//    Assert.assertEquals(3, solution.numRescueBoats(new int[]{1, 3, 3, 4, 5}, 8));
+    Assert.assertEquals(53, solution.numRescueBoats(new int[]{8, 3, 8, 3, 10, 2, 9, 1, 3, 6, 6, 4, 2, 3, 3, 8, 10, 6,
+      1, 8, 4, 4, 6, 3, 10, 2, 5
+      , 3, 6, 6, 7, 6, 5, 7, 5, 8, 8, 3, 4, 7, 2, 7, 4, 6, 2, 7, 4, 5, 5, 5, 7, 4, 7, 1, 4, 8, 1, 7, 1, 5, 9, 1, 6, 1
+      , 9, 7, 8, 7, 1, 1, 7, 10, 9, 7, 8, 3, 8, 3, 2, 5, 4, 2
+      , 5, 9, 5, 5, 8, 6, 2, 10, 5, 8, 4, 9, 4, 3, 2, 10, 6, 1}, 10));
+  }
+
+  @Test
   public void fairCandySwap() {
     Assert.assertArrayEquals(new int[]{1, 2}, solution.fairCandySwap(new int[]{1, 1}, new int[]{2, 2}));
     Assert.assertArrayEquals(new int[]{1, 2}, solution.fairCandySwap(new int[]{1, 2}, new int[]{2, 3}));
