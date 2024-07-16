418b889893b5ee25145f24285926db4ce076a2cd
leopold.zw <1280252178@qq.com>
2024-07-16 11:58:22 +0800

    解决第 2956 题

--- src/main/java/com/wen/repository/solution2900To2999/Solution2950To2959.java
+++ src/main/java/com/wen/repository/solution2900To2999/Solution2950To2959.java
@@ -1,7 +1,9 @@
 package com.wen.repository.solution2900To2999;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Solution2950To2959 {
 	/**
@@ -24,6 +26,33 @@ public class Solution2950To2959 {
 				result.add(i);
 			}
 		}
+		return result;
+	}
+
+	/**
+	 * 2956. Find Common Elements Between Two Arrays
+	 *
+	 * <p>n == nums1.length</p>
+	 * <p>m == nums2.length</p>
+	 * <p>1 <= n, m <= 100</p>
+	 * <p>1 <= nums1[i], nums2[i] <= 100</p>
+	 */
+	public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+		Map<Integer, Integer> map = new HashMap<>();
+		int[] result = new int[]{0, 0};
+
+		for (int num : nums1) {
+			map.put(num, map.getOrDefault(num, 0) + 1);
+		}
+
+		for (int num : nums2) {
+			if (map.containsKey(num)) {
+				result[1]++;
+				result[0] += map.get(num);
+				map.put(num, 0);
+			}
+		}
+
 		return result;
 	}
 }
--- src/test/java/com/wen/repository/solution2900To2999/Solution2950To2959Test.java
+++ src/test/java/com/wen/repository/solution2900To2999/Solution2950To2959Test.java
@@ -13,4 +13,14 @@ public class Solution2950To2959Test {
 		Assert.assertArrayEquals(new Integer[]{}, solution.findPeaks(new int[]{2, 4, 4}).toArray());
 		Assert.assertArrayEquals(new Integer[]{1, 3}, solution.findPeaks(new int[]{1, 4, 3, 8, 5}).toArray());
 	}
+
+	@Test
+	public void findIntersectionValues() {
+		Assert.assertArrayEquals(new int[]{2, 1},
+			solution.findIntersectionValues(new int[]{2, 3, 2}, new int[]{1, 2}));
+		Assert.assertArrayEquals(new int[]{3, 4},
+			solution.findIntersectionValues(new int[]{4, 3, 2, 3, 1}, new int[]{2, 2, 5, 2, 3, 6}));
+		Assert.assertArrayEquals(new int[]{0, 0},
+			solution.findIntersectionValues(new int[]{3, 4, 2, 3}, new int[]{1, 5}));
+	}
 }
